af6d270ba9deac6376c89ee42eff17d00d924899
vimalpksh <itisvimal@gmail.com>
2023-01-01 23:55:52 +0530

    Refactored code
    Refactored code again to implement functions inside the class.

--- script.js
+++ script.js
@@ -20,11 +20,17 @@ class App{
 
     constructor(){
         this._getPosition();
+
+        form.addEventListener("submit", fthis._newWorkout);
+
+        inputType.addEventListener("change", function(){
+            inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+            inputCadence.closest(".form__row").classList.toggle("form__row--hidden")
     }
 
     _getPosition(){
         if(navigator.geolocation)
-navigator.geolocation.getCurrentPosition(this._loadMap,function(){
+navigator.geolocation.getCurrentPosition(this._loadMap.bind(this),function(){
     alert(`Could not get your position`)
 } )
 
@@ -58,35 +64,32 @@ navigator.geolocation.getCurrentPosition(this._loadMap,function(){
 
     _toggleElevationField(){}
 
-    _newWorkout(){}
+    _newWorkout(){
+        e.preventDefault(e);
+        //Clear input columns
+        
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = " ";
+        
+        //display marker
+        
+             console.log(this.#mapEvent);
+        const {lat,lng} = mapEvent.latlng;
+            L.marker([lat, lng]).addTo(this.#map).bindPopup(L.popup({
+                maxWidth:250, 
+                minWidth:100,
+                autoClose: false,
+                closeOnClick : false,
+                className: "running-popup",
+        
+            })).setPopupContent("Workout").openPopup(); 
+        
+        })
+        
+        
+    }
 }
 const app = new App();
 
 
 
 
-form.addEventListener("submit", function(e){
-    e.preventDefault();
-//Clear input columns
-
-inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = " ";
-
-//display marker
-
-     console.log(mapEvent);
-const {lat,lng} = mapEvent.latlng;
-    L.marker([lat, lng]).addTo(map).bindPopup(L.popup({
-        maxWidth:250, 
-        minWidth:100,
-        autoClose: false,
-        closeOnClick : false,
-        className: "running-popup",
-
-    })).setPopupContent("Workout").openPopup(); 
-
-})
-
-inputType.addEventListener("change", function(){
-    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-    inputCadence.closest(".form__row").classList.toggle("form__row--hidden")
-})
