4a97ae97220348ebd36f8e129c6fd804f896fd11
vimalpksh <itisvimal@gmail.com>
2023-01-02 20:18:37 +0530

    Created classes for workout data

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 class Workout {
 date = new Date();
+id = (Date.now()+ "").slice(-10);
 
 
     constructor(coords, distance, duration) {
@@ -22,8 +23,36 @@ date = new Date();
     }
 }
 
+class Running extends Workout{
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+    calcPace(){
+        this.pace = this.duration / this.distance;
+        return this.pace;
 
+    }
+}
+class Cycling extends Workout{
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+    }
+    calcSpeed() {
+        this.speed = this.distance / (this.duration/60);
+        return this.speed;
 
+    }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cyc1 = new Cycling([39, -12], 27, 96, 523);
+console.log(run1,cyc1);
+/////////////////////////////////////////////////////////
+//APPLICATION ARCHITECTURE
 class App{
     #map;
     #mapEvent;
